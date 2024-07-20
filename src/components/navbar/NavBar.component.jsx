f0c1366b2fb3386fb1e9b7cac5c4d068312bdb5c
devlogex <tannd.devlogex@gmail.com>
2024-07-20 23:14:56 +0700

    sticky navbar

--- src/components/navbar/NavBar.component.jsx
+++ src/components/navbar/NavBar.component.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const NavBar = ({ logo = "", items = [] }) => {
-  const [isNavLinkVisible, setIsNavLinkVisible] = React.useState(false);
+  const [isNavLinkVisible, setIsNavLinkVisible] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
   const handleBurgerClick = () => {
     setIsNavLinkVisible(!isNavLinkVisible);
   };
 
+  const handleItemClick = () => {
+    setIsNavLinkVisible(false);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        // Scroll Down
+        setShowNavbar(false);
+      } else {
+        // Scroll Up
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   const navItems = items.map((item, index) => (
-    <li key={index}>
+    <li key={index} onClick={handleItemClick}>
       <a href={item.link}>{item.name}</a>
     </li>
   ));
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${showNavbar ? "show" : "hide"}`}>
       <div className="logo">
         <a href="/">{logo}</a>
       </div>
