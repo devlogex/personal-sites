65797cc9a7f03b7d023aff34f5c8778d515d7c09
devlogex <tannd.devlogex@gmail.com>
2024-07-20 22:58:07 +0700

    refactor code navbar

--- src/components/navbar/NavBar.component.jsx
+++ src/components/navbar/NavBar.component.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import './NavBar.style.css';
+import React from "react";
+import "./style.css";
 
-const NavBar = ({ logo = '', items = [] }) => {
-    const [isNavLinkVisible, setIsNavLinkVisible] = React.useState(false);
-    const handleBurgerClick = () => {
-        setIsNavLinkVisible(!isNavLinkVisible);
-    }
+const NavBar = ({ logo = "", items = [] }) => {
+  const [isNavLinkVisible, setIsNavLinkVisible] = React.useState(false);
+  const handleBurgerClick = () => {
+    setIsNavLinkVisible(!isNavLinkVisible);
+  };
 
-    const navItems = items.map((item, index) =>  (
-            <li key={index}><a href={item.link}>{item.name}</a></li>
-    ));
+  const navItems = items.map((item, index) => (
+    <li key={index}>
+      <a href={item.link}>{item.name}</a>
+    </li>
+  ));
 
-    return (
-        <nav className="navbar">
-            <div className="logo"><a href='/'>{logo}</a></div>
-            <ul className={`nav-links ${isNavLinkVisible ? 'nav-active' : ''}`}>
-                {navItems}
-            </ul>
-            <div className="burger" onClick={handleBurgerClick}>
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/">{logo}</a>
+      </div>
+      <ul className={`nav-links ${isNavLinkVisible ? "nav-active" : ""}`}>
+        {navItems}
+      </ul>
+      <div className="burger" onClick={handleBurgerClick}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
