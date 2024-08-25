3ae8990012b0044a7e7ecb7eef6d2eb99aa693d0
devlogex <tannd.devlogex@gmail.com>
2024-08-25 16:43:55 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import { Footer, NavBar } from "@/components";
-import { AboutMe } from "@/pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BlogList, BlogPost } from "./containers";
+import { Header } from "@/components";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <NavBar
-            logo="DEVLOGEX"
-            items={[
-              { name: "Blog", link: "/blog" },
-              { name: "About", link: "/about" },
-            ]}
-          />
+          <Header />
           <Routes>
-            <Route path="/" element={<BlogList />} />
-            <Route path="/about" element={<AboutMe />} />
-            <Route path="/blog" element={<BlogList />} />
-            <Route path="/blog/:postId" element={<BlogPost />} />
+            <Route path="/" element={<div>Hello</div>} />
           </Routes>
-          <Footer />
         </div>
       </Router>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as ButtonLink } from "./Button/ButtonLink.component";
 export { default as NavBar } from "./Navbar/NavBar.component";
 export { default as Loading } from "./Loading/Loading.component";
 export { default as Footer } from "./Footer/Footer.component";
+export { default as Header } from "./Header/Header.component";
